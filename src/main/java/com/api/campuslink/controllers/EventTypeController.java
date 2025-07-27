c03e73b553ddcd229335ff0844d058be9f8cb4ec
sanketkumar <rajkumarsanket4@gmail.com>
2025-07-27 18:00:52 +0530

    Added update and delete API for EventType

--- src/main/java/com/api/campuslink/controllers/EventTypeController.java
+++ src/main/java/com/api/campuslink/controllers/EventTypeController.java
@@ -24,26 +24,40 @@ public class EventTypeController {
     @PostMapping("/add")
     public ResponseEntity<?> insertEventType(@RequestBody @Valid EventTypeRequestDTO eventTypeRequestDTO) {
         log.info("Got request to add new event type");
-
         Result<EventType> result = eventTypeService.addEventType(eventTypeRequestDTO);
-
         if (!result.isSuccess()) {
             return new ResponseEntity<>("Unable to save event type " + eventTypeRequestDTO.getName() + " due to " + result.getError(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
         return new ResponseEntity<>("Event type " + eventTypeRequestDTO.getName() + " saved succesfully.", HttpStatus.OK);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<?> getAllEventType(@RequestParam(defaultValue = "id") String sortBy,
-                                             @RequestParam(defaultValue = "true") boolean asc) {
+    public ResponseEntity<?> getAllEventType(@RequestParam(defaultValue = "id") String sortBy, @RequestParam(defaultValue = "true") boolean asc) {
         log.info("Fetching all the event type.");
         Result<List<EventType>> result = this.eventTypeService.getAllEventType(sortBy, asc);
-
         if (result.isSuccess()) {
             return new ResponseEntity<>(result.getData(), HttpStatus.OK);
         }
-     log.info("hello world help me in this situation");return new ResponseEntity<>("Unable to fetch the event types", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>("Unable to fetch the event types", result.getHttpStatus());
+    }
 
+    @PutMapping("/update")
+    public ResponseEntity<?> updateEventType(@RequestBody @Valid EventTypeRequestDTO eventTypeRequestDTO) {
+        log.info("Got request to update event type with code: {}", eventTypeRequestDTO.getEventCode());
+        Result<EventType> result = this.eventTypeService.updateEventType(eventTypeRequestDTO);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getData(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Unable to update event type " + eventTypeRequestDTO.getName() + " due to " + result.getError(), result.getHttpStatus());
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteEventType(@RequestParam Long id) {
+        log.info("Got request to delete event type with code: {}", id);
+        Result<String> result = this.eventTypeService.deleteEventType(id);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getData(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Unable to delete event type with code " + id + " due to " + result.getError(), result.getHttpStatus());
     }
 }
